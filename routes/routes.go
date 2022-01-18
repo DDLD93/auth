deda5c4d7b34c74e7770b5deb91c8fbc151d5035
ddld93 <umar.jere@gmail.com>
2022-01-18 16:59:39 +0100

    cors again

--- routes/routes.go
+++ routes/routes.go
@@ -24,6 +24,7 @@ type CustomResponse struct {
 func (ur *UserRoute) CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers","Content-Type,access-control-allow-origin, access-control-allow-headers")
 	user := model.User{}
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
