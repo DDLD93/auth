e8753d12103ea2239abe47b06a924fda1ce6d794
ddld93 <umar.jere@gmail.com>
2022-01-21 13:39:47 +0100

    bb

--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ require (
 )
 
 require (
+	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
-	github.com/joho/godotenv v1.4.0
 	github.com/o1egl/paseto v1.0.0
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -9,25 +8,31 @@ import (
 	"github.com/ddld93/auth/routes"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
 )
 
 
 func main() {
-	port := "5000"
+	//port := "5000"
 	userCtrl := controller.NewUserCtrl("mongo", 27017)
 	route := routes.UserRoute{UserCtrl: userCtrl}
 	r := mux.NewRouter()
 
+	credentials := handlers.AllowCredentials()
+	methods := handlers.AllowedMethods([]string{"POST"})
+	//ttl := handlers.MaxAge(3600)
+	origins := handlers.AllowedOrigins([]string{"*"})
 	// router handlers
 	r.HandleFunc("/api/v1/auth/login", route.Login).Methods("POST")
 	r.HandleFunc("/api/v1/auth/signup", route.CreateUser).Methods("POST")
 	//r.HandleFunc("/user", route.GetUser).Methods("GET")
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./www/")))
-	r.Use(mux.CORSMethodMiddleware(r))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("www")))
+	
 
-	fmt.Printf("Server listening on port %v", port)
-	if err := http.ListenAndServe(":"+port, r); err != nil {
-		log.Fatal("Error starting server !! ", err)
-	}
+	// fmt.Printf("Server listening on port %v", port)
+	// if err := http.ListenAndServe(":"+port, r); err != nil {
+	// 	log.Fatal("Error starting server !! ", err)
+	// }
+	log.Fatal(http.ListenAndServe(":5000", handlers.CORS(credentials, methods, origins)(r)))
 
 }
