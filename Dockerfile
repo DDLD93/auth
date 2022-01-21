84b88fd8f8968b72fe5bab987b48669026786063
ddld93 <umar.jere@gmail.com>
2022-01-21 09:13:26 +0100

    docker build

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM golang:alpine AS build-env
+RUN apk --no-cache add build-base git bzr mercurial gcc
+ADD . /src
+RUN cd /src && go build -o authServer
+
+# final stage
+FROM alpine
+WORKDIR /app
+COPY --from=build-env /src/authServer /app/
+ENTRYPOINT ./authServer
--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/ddld93/auth/controller"
 	"github.com/ddld93/auth/routes"
+
 	//utilities "github.com/ddld93/auth/utilities"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
+
 type MiddlewareFunc func(http.Handler) http.Handler
 
 func init() {
@@ -20,29 +21,27 @@ func init() {
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
-}
+	}
 }
 
-func main()  {
-	port := os.Getenv("PORT")
-	userCtrl := controller.NewUserCtrl("localhost", 27017)
+func main() {
+	port := "5000"
+	userCtrl := controller.NewUserCtrl("mongo", 27017)
 	route := routes.UserRoute{UserCtrl: userCtrl}
 	// var TokenMaker,_ = NewPasetoMaker("tfgrfdertygtrfdewsdftgyhujikolpy") // secrete must be 32 bit char
-	
-//comitt
+
+	//comitt
 	r := mux.NewRouter()
-	
-    r.HandleFunc("/api/v1/auth/login",route.Login ).Methods("POST")
-    r.HandleFunc("/api/v1/auth/signup",route.CreateUser ).Methods("POST")
-	//r.HandleFunc("/user", route.GetUser).Methods("GET") 
+
+	r.HandleFunc("/api/v1/auth/login", route.Login).Methods("POST")
+	r.HandleFunc("/api/v1/auth/signup", route.CreateUser).Methods("POST")
+	//r.HandleFunc("/user", route.GetUser).Methods("GET")
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./www/")))
 	r.Use(mux.CORSMethodMiddleware(r))
-
 
 	fmt.Printf("Server listening on port %v", port)
 	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatal("Error starting server !! ", err)
 	}
-
 
 }
